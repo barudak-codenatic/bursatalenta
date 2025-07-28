41972729d2496b279979c33bcd91ec90d3ed173e
dindinmhs <dindin.imanudin@mhs.itenas.ac.id>
2025-07-28 21:27:47 +0700

    route login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,8 @@ const userController = require('../controllers/userController');
 router.post('/register', userController.register);
 router.get('/:id', userController.getUser);
 router.put('/:id', userController.updateUser);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+router.get("/me", userController.checkSession);
 
 module.exports = router;
